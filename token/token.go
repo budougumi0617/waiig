87886fb84fa4ee8c1510b511b57145e4f1ffea5f
budougumi0617 <budougumi0617@gmail.com>
2018-06-18 22:55:22 +0900

    :+1: 1.2 Add defines of token types

--- token/token.go
+++ token/token.go
@@ -3,8 +3,54 @@ package token
 // TokenType defines token type in Monkey.
 type TokenType string
 
-// Token is a token object
+// Token is a token object.
 type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	// ILLEGAL means unknown keyword.
+	ILLEGAL = "ILLEGAL"
+	// EOF means "End Of File".
+	EOF = "EOF"
+
+	// Identifiers + literals
+	// IDENT means identifiers.
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	// INT means number.
+	INT = "INT" // 1343456
+
+	// Operators
+	// ASSIGN means "="
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
