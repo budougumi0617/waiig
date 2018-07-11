30ee26c3f2e880439b276233e7157b2599396db2
budougumi0617 <budougumi0617@gmail.com>
2018-07-11 10:29:25 +0900

    :+1: P32 2.4 Add Node implemets

--- ast/ast.go
+++ ast/ast.go
@@ -17,3 +17,16 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// Program imprements Node interface.
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns literal.
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
