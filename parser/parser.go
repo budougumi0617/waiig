1c22042491deb76956e1194840c6f728c839d438
budougumi0617 <budougumi0617@gmail.com>
2018-08-06 23:00:00 +0900

    :+1: P59 2.6.6 Add parseExpression

--- parser/parser.go
+++ parser/parser.go
@@ -8,6 +8,24 @@ import (
 	"github.com/budougumi0617/waiig/token"
 )
 
+const (
+	_ int = iota
+	// LOWEST is minimum priority
+	LOWEST
+	// EQUALS is ==
+	EQUALS
+	// LESSGREATER is > or <
+	LESSGREATER
+	// SUM is +
+	SUM
+	// PRODUCT is *
+	PRODUCT
+	// PREFIX is -X or !X
+	PREFIX
+	// CALL is  myFunction(X)
+	CALL // myFunction(X)
+)
+
 type (
 	prefixParseFn func() ast.Expression
 	infixParseFn  func(ast.Expression) ast.Expression
@@ -67,7 +85,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -103,6 +121,28 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
 }
 
 func (p *Parser) curTokenIs(t token.Type) bool {
